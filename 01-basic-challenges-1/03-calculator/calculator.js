2cad37eb70a79a1aad7b0d89f8793ef2a15093aa
Prabesh <tandukarprabesh@gmail.com>
2024-08-29 14:27:35 +1200

    1.03

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      throw new Error("Invalid Operator");
+  }
+}
 
 module.exports = calculator;
