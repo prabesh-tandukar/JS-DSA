375863bf6ec900ea61d44c9928f822c087d25d13
Prabesh <tandukarprabesh@gmail.com>
2024-08-30 23:41:45 +1200

    find max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
@@ -1,5 +1,6 @@
-const findMaxNumber = require('./find-max-number');
+const findMaxNumber = require("./find-max-number");
 
-const result = findMaxNumber([2, 1, 9, 16, 10]);
+// const result = findMaxNumber([2, 1, 9, 16, 10]);
+const result = findMaxNumber([22, 33, 44, 55, 11, 66, 96]);
 
 console.log(result);
--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  let length = arr.length;
+  let max = 0;
+  for (let i = 0; i < length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
 module.exports = findMaxNumber;
