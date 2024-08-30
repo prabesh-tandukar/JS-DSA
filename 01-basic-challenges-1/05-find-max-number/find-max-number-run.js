01ce52f1ddbdd1aa94770a6b51fa02cd8283a66e
Prabesh <tandukarprabesh@gmail.com>
2024-08-30 23:42:33 +1200

    find  max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
@@ -2,5 +2,6 @@ const findMaxNumber = require("./find-max-number");
 
 // const result = findMaxNumber([2, 1, 9, 16, 10]);
 const result = findMaxNumber([22, 33, 44, 55, 11, 66, 96]);
+n;
 
 console.log(result);
