a8c67816340020e2db6e382ba11d0f89fcd4376c
Prabesh <tandukarprabesh@gmail.com>
2024-09-01 23:38:49 +1200

    palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,20 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+  const og = str.toLowerCase();
+  let theRealOg = "";
+  for (let i = 0; i < og.length; i++) {
+    // console.log(og[i]);
+    if (og[i] == " " || og[i] == ",") {
+      continue;
+    }
+    theRealOg += og[i];
+  }
+  //   console.log(theRealOg);
+  let reversed = "";
+  for (let i = theRealOg.length - 1; i >= 0; i--) {
+    reversed += theRealOg[i];
+  }
+
+  return theRealOg == reversed;
+}
 
 module.exports = isPalindrome;
