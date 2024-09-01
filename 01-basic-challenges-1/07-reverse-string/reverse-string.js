06de30d4f1d8c53588648eea2252e31bf16af4c3
Prabesh <tandukarprabesh@gmail.com>
2024-09-01 23:15:22 +1200

    reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,10 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversed = "";
+  let len = str.length;
+  for (let i = len - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
 module.exports = reverseString;
