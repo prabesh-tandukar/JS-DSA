823bdbbcecb85c7dccffecb96937f378fbfebc19
Prabesh <tandukarprabesh@gmail.com>
2024-09-04 22:17:08 +1200

    count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,15 @@
-function countVowels() {}
+function countVowels(str) {
+  let lowerStr = str.toLowerCase();
+  let count = 0;
+  const vowels = "aeiou";
+  for (let i = 0; i < lowerStr.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (lowerStr[i] === vowels[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
 
 module.exports = countVowels;
