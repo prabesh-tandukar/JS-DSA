154f6a5276fe5f09e1f848c8b0b1895b8ff73ec6
Prabesh <tandukarprabesh@gmail.com>
2024-09-05 23:05:42 +1200

    dupli

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,7 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  let beforeArr = arr;
+  let afterArr;
+  for (let i = 0; i < beforeArr.length; i++) {}
+}
 
 module.exports = removeDuplicates;
